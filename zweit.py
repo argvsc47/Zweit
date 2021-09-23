fbf718b94f1febbb6910ae4adc3a1c8fc328e70d
argvsc47 <55107887+argvsc47@users.noreply.github.com>
2021-09-23 17:17:29 +0100

    Update zweit.py

--- zweit.py
+++ zweit.py
@@ -9,17 +9,33 @@ if len(argv) == 1:
 with open(argv[1]) as f:
 	lines = f.read().split("\n")
 
-if len(lines) < 6:
-	exit("Wrong formatted build file")
-
 init()
 
+lidx = 0
+
 COMPILER = lines[0]
-FLAGS = lines[1]
-SRC = lines[3]
+lidx = 1
+
+while lines[lidx] == "":
+	lidx += 1
+
+FLAGS = "" if lines[lidx] == "!NONE" else lines[lidx]
+lidx += 1
+
+while lines[lidx] == "":
+	lidx += 1
+
+SRC = "" if lines[lidx] == "!NONE" else lines[lidx]
+lidx += 1
+
+while lines[lidx] == "":
+	lidx += 1
+
 FILES = []
 
-for file in lines[5:]:
+for file in lines[lidx:]:
+	if file == "":
+		continue
 	FILES.append(file)
 
 total = 0
@@ -30,10 +46,11 @@ for file in FILES:
 	print(f"- Compiling file: {file}, path: {SRC+file}")
 	start = time()
 	if system(f"{COMPILER} {SRC+file} {FLAGS}") != 0:
+		end = time()
 		print(f"* Compiling file: {Fore.RED}{file} FAILED{Fore.RESET}")
 	else:
 		end = time()
 		print(f"+ File Compiled: {Fore.CYAN}{file}{Fore.RESET}, in: {Fore.GREEN}{end-start}s{Fore.RESET}")
-		total += end-start
+	total += end-start
 
 print(f"Built {Fore.YELLOW}{argv[1]}{Fore.RESET} in: {Fore.GREEN}{total}s{Fore.RESET}")
