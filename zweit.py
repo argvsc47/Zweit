5369381ac8eecffceeba5c78dc28fd0c21795cd6
argvsc47 <55107887+argvsc47@users.noreply.github.com>
2021-09-23 18:06:22 +0100

    Update zweit.py

--- zweit.py
+++ zweit.py
@@ -7,50 +7,94 @@ if len(argv) == 1:
 	exit("Too few arguments")
 
 with open(argv[1]) as f:
-	lines = f.read().split("\n")
+	lns = f.read().split("\n")
 
 init()
 
-lidx = 0
+def make_command(lines):
+	lidx = 0
 
-COMPILER = lines[0]
-lidx = 1
+	COMPILER = lines[0]
+	lidx = 1
 
-while lines[lidx] == "":
+	while lines[lidx] == "":
+		lidx += 1
+
+	FLAGS = "" if lines[lidx] == "!NONE" else lines[lidx]
 	lidx += 1
 
-FLAGS = "" if lines[lidx] == "!NONE" else lines[lidx]
-lidx += 1
+	while lines[lidx] == "":
+		lidx += 1
 
-while lines[lidx] == "":
+	SRC = "" if lines[lidx] == "!NONE" else lines[lidx]
 	lidx += 1
 
-SRC = "" if lines[lidx] == "!NONE" else lines[lidx]
-lidx += 1
+	while lines[lidx] == "":
+		lidx += 1
 
-while lines[lidx] == "":
-	lidx += 1
+	FILES = []
 
-FILES = []
+	for file in lines[lidx:]:
+		if file == "":
+			continue
+		FILES.append(file)
 
-for file in lines[lidx:]:
-	if file == "":
+	return {
+		"compiler": COMPILER,
+		"flags": FLAGS,
+		"source": SRC,
+		"files": FILES
+	}
+
+def build(cfg):
+	COMPILER = cfg["compiler"]
+	FLAGS = cfg["flags"]
+	SRC = cfg["source"]
+	FILES = cfg["files"]
+
+	total = 0
+
+	print(f"Building {Fore.YELLOW}{argv[1]}{Fore.RESET}")
+
+	for file in FILES:
+		print(f"- Compiling file: {file}, path: {SRC+file}")
+		start = time()
+		if system(f"{COMPILER} {SRC+file} {FLAGS}") != 0:
+			end = time()
+			print(f"* Compiling file: {Fore.RED}{file} FAILED{Fore.RESET}")
+		else:
+			end = time()
+			print(f"+ File Compiled: {Fore.CYAN}{file}{Fore.RESET}, in: {Fore.GREEN}{end-start}s{Fore.RESET}")
+		total += end-start
+
+	print(f"Built {Fore.YELLOW}{argv[1]}{Fore.RESET} in: {Fore.GREEN}{total}s{Fore.RESET}")
+
+data = []
+NEW = False
+offset = 0
+name = "default"
+batch = []
+
+for _, ln in enumerate(lns):
+	if ln.startswith("#"):
+		data.append(
+			[name, batch]
+		)
+		name = ln[2:]
+		offset = _+1
+		batch = []
 		continue
-	FILES.append(file)
 
-total = 0
+	batch.append(ln)
 
-print(f"Building {Fore.YELLOW}{argv[1]}{Fore.RESET}")
+if batch:
+	data.append([name, batch])
 
-for file in FILES:
-	print(f"- Compiling file: {file}, path: {SRC+file}")
-	start = time()
-	if system(f"{COMPILER} {SRC+file} {FLAGS}") != 0:
-		end = time()
-		print(f"* Compiling file: {Fore.RED}{file} FAILED{Fore.RESET}")
-	else:
-		end = time()
-		print(f"+ File Compiled: {Fore.CYAN}{file}{Fore.RESET}, in: {Fore.GREEN}{end-start}s{Fore.RESET}")
-	total += end-start
+cmds = {}
+for batch in data:
+	cmds.update({batch[0]:make_command(batch[1])})
 
-print(f"Built {Fore.YELLOW}{argv[1]}{Fore.RESET} in: {Fore.GREEN}{total}s{Fore.RESET}")
+if len(argv) == 2:
+	build(cmds["default"])
+else:
+	build(cmds[argv[2]])
